bcca29cc85c00bde2bc440e129c18185e5507bce
alexmanto <40876399+alexmanto@users.noreply.github.com>
2020-02-11 23:38:55 -0300

    Criação do domain Entity e das entidades Produto e Categoria, sendo a entidade Produto uma entidade agregadora.

---
+++ src/TestStore.Catalogo.Domain/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestStore.Core.DomainObjects;
+
+namespace TestStore.Catalogo.Domain
+{
+    public class Categoria : Entity
+    {
+        public string Nome { get; private set; }
+
+        public int Codigo { get; private set; }
+
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+    }
+}
---
+++ src/TestStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using TestStore.Core.DomainObjects;
+
+namespace TestStore.Catalogo.Domain
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        public string Nome { get; private set; }
+
+        public string Descricao { get; private set; }
+
+        public bool Ativo { get; private set; }
+
+        public decimal Valor { get; private set; }
+
+        public DateTime DataCadastro { get; private set; }
+
+        public string Imagem { get; private set; }
+
+        public int QuantidadeEstoque { get; private set; }
+
+        public Categoria Categoria { get; private set; }
+
+        public Guid CategoriaId { get; private set; }
+
+        public Produto(string nome, string descricao, bool ativo, decimal valor, DateTime dataCadastro, string imagem, Guid categoriaId)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new Exception("A nome informado não pode ser nulo.");
+            if (string.IsNullOrEmpty(descricao))
+                throw new Exception("A descrição informada não pode ser nula.");
+            if (valor <= 0)
+                throw new Exception("A valor informado deve ser mais que zero.");
+
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+            CategoriaId = categoriaId;
+        }
+
+        public void Ativar() => Ativo = true;
+
+        public void Desativar() => Ativo = false;
+
+        public void AlterarCategoria(Categoria categoria)
+        {
+            Categoria = categoria;
+            CategoriaId = categoria.Id;
+        }
+
+        public void AlterarDescricao(string descricao)
+        {
+            if (string.IsNullOrEmpty(descricao))
+                throw new Exception("A descrição informada não pode ser nula.");
+
+            Descricao = descricao;
+        }
+
+        public void DebitarEstoque(int quantidade)
+        {
+            if (quantidade < 0)
+                quantidade *= -1;
+
+            QuantidadeEstoque -= quantidade;
+        }
+
+        public void ReporEstoque(int quantidade)
+        {
+            if (quantidade <= 0)
+                throw new Exception("A quantidade informada para repor estoque deve ser maior que zero.");
+
+            QuantidadeEstoque += quantidade;
+        }
+
+        public bool PossuiEstoqueSuficiente(int quantidade)
+        {
+            if (quantidade <= 0)
+                throw new Exception("A quantidade informada para repor estoque deve ser maior que zero.");
+
+            return QuantidadeEstoque >= quantidade;
+        }
+
+        public void Validar()
+        {
+
+        }
+    }
+}
